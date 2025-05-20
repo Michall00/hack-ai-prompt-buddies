39ab082be90fbfe2e4da57ca09981e30466318b1
misadowsk <michalsad.protondynamic@gmail.com>
2025-05-20 14:26:03 +0200

    feat: added sliding window with last k messages

--- src/prompt_genaration/prompt_generator.py
+++ src/prompt_genaration/prompt_generator.py
@@ -24,7 +24,6 @@ class PromptGenerator:
         self.model = model
         self.tools = tools
         self.system_message = None
-        self.tools = tools
 
     def generate_first_prompt(
         self,
